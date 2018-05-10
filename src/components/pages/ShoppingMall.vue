2685c2052577915046b0ca17302a765d73506309
wjf <wangjianfeng@soundbus.cn>
2018-05-10 11:40:06 +0800

    09 商品推荐vue-awesome-swiper

--- src/components/pages/ShoppingMall.vue
+++ src/components/pages/ShoppingMall.vue
@@ -30,20 +30,47 @@
         </div>
         <!--AD banner area-->
         <div class="ad-banner">
-            <img v-lazy="adBanner.PICTURE_ADDRESS" width="100%">
+            <img v-lazy="adBanner" width="100%">
+        </div>
+        <!--Recommend goods area-->
+        <div class="recommend-area">
+            <div class="recommend-title">
+                商品推荐
+            </div>
+            <div class="recommend-body">
+                <swiper :options="swiperOptions" >
+                    <swiper-slide v-for="( item , index ) in recommendGoods" :key="index">
+                        <div class="recommend-item">
+                            <img :src="item.image" width="100%">
+                            <div>{{ item.goodsName }}</div>
+                            <div>￥{{item.price}} (￥{{item.mallPrice}})</div>
+                        </div>
+                    </swiper-slide>
+                </swiper>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
     import axios from 'axios'
+    import 'swiper/dist/css/swiper.css'
+    import { swiper, swiperSlide } from 'vue-awesome-swiper'
+
     export default {
+        components: {
+            swiper, swiperSlide
+        },
         data() {
             return {
                 locationIcon: require('../../assets/images/locationa.png'),
                 bannerPicArray:[],
                 category:[],
-                adBanner:''
+                adBanner:'',
+                recommendGoods:[],
+                swiperOptions:{
+                    slidesPerView: 3
+                },
             }
         },
         created() {
@@ -58,14 +85,18 @@
                     //获取分类
                     this.category = data.category; 
                     //获取广告图片
-                    this.adBanner = data.advertesPicture;   
+                    this.adBanner = data.advertesPicture.PICTURE_ADDRESS;   
                     //轮播图
-                    this.bannerPicArray = data.slides
+                    this.bannerPicArray = data.slides;
+                    //推荐商品
+                    this.recommendGoods = data.recommend;
                 }
             })
             .catch(error =>{
                 console.log(error);
             }) 
+        },
+        methods: {
         }
     }
 </script>
@@ -75,6 +106,7 @@
         height: 2.2rem;
         background-color: #e5017d;
         line-height:2.2rem;
+        overflow: hidden;
     }
     .search-input{
         width: 100%;
@@ -92,7 +124,7 @@
     }
     .swiper-area{
         clear: both;
-        max-height: 12rem;
+        height: 8rem;
         overflow: hidden;
     }
     .type-bar{
@@ -108,5 +140,21 @@
         padding: .3rem;
         font-size: 12px;
         text-align: center;
+    }
+    .recommend-area{
+       background-color: #fff;
+       margin-top: .3rem;
+    }
+    .recommend-title{
+        border-bottom:1px solid #eee;
+        font-size:14px;
+        padding:.2rem;
+        color:#e5017d;
+    }
+    .recommend-item{
+      width:99%;
+      border-right: 1px solid #eee;
+      font-size: 12px;
+      text-align: center;
     }
 </style>
